e552b2d4c2e498e386ad3a7c9919872192fb6a7c
solozyx <zhaoyingxin@aliyun.com>
2020-12-18 15:18:59 +0800

    go-zero rpc mysql redis

--- rpc/add/internal/svc/servicecontext.go
+++ rpc/add/internal/svc/servicecontext.go
@@ -1,13 +1,20 @@
 package svc
 
-import "go-zero-demo/rpc/add/internal/config"
+import (
+	"github.com/tal-tech/go-zero/core/stores/sqlx"
+
+	"go-zero-demo/rpc/add/internal/config"
+	"go-zero-demo/rpc/model"
+)
 
 type ServiceContext struct {
 	c config.Config
+	Model model.BookModel   // 手动代码
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
 		c: c,
+		Model: model.NewBookModel(sqlx.NewMysql(c.DataSource), c.Cache), // 手动代码
 	}
 }
---
+++ rpc/model/book.sql
@@ -0,0 +1,11 @@
+
+create database gozero;
+
+use gozero;
+
+CREATE TABLE `book`
+(
+  `book` varchar(255) NOT NULL COMMENT 'book name',
+  `price` int NOT NULL COMMENT 'book price',
+  PRIMARY KEY(`book`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
