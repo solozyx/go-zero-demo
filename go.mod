6f2eaa4c1a938924e662c17c901fcc6f51d1c0d0
solozyx <zhaoyingxin@aliyun.com>
2020-12-18 14:35:13 +0800

    go-zero rpc add

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module go-zero-demo
 
 go 1.14
 
-require github.com/tal-tech/go-zero v1.1.1
+require (
+	github.com/golang/protobuf v1.4.2
+	github.com/tal-tech/go-zero v1.1.1
+	google.golang.org/grpc v1.29.1
+)
---
+++ rpc/add/internal/svc/servicecontext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "go-zero-demo/rpc/add/internal/config"
+
+type ServiceContext struct {
+	c config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		c: c,
+	}
+}
